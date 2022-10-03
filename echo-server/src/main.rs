960ce9ecc9cf9db578d6a5d5cabfaec466ca3ca4
Dag Andersen <dagbjerreandersen@gmail.com>
2022-10-03 12:22:10 +0200

    Added delay for response

--- echo-server/src/main.rs
+++ echo-server/src/main.rs
@@ -6,13 +6,24 @@ use warp::Filter;
 #[derive(Debug, StructOpt)]
 #[structopt(name = "echo-client")]
 struct Opt {
+    /// Version name. This will be returned to the client.
     #[structopt(long, env, default_value = "unknown")]
     version: String,
+
+    /// minimum delay between responses. The actual delay will be between (delay_min) and (delay_min + delay_random).
+    #[structopt(long, env, default_value = "0")]
+    delay_min: u64,
+
+    /// random delay between responses. The actual delay will be between (delay_min) and (delay_min + delay_random).
+    #[structopt(long, env, default_value = "0")]
+    delay_random: u64,
 }
 
 #[tokio::main]
 async fn main() {
     let opt = Opt::from_args();
+    let delay_min = opt.delay_min;
+    let delay_random = opt.delay_random;
     let version = opt.version.as_str().to_string();
     let address = get_addresses(3030);
     // generated random name:
@@ -22,6 +33,17 @@ async fn main() {
     // GET /hello/warp => 200 OK with body "Hello, warp!"
     let hello = warp::path!("hello" / String).map(move |name| {
         println!("### I, {}, was called by a client with name: {}", &server_name, name);
+        // random interval between 0 and 500 miliseconds
+        let interval = if delay_random == 0 {
+            delay_min
+        } else {
+            delay_min + rand::thread_rng().gen_range(0..delay_random)
+        };
+
+        if interval > 0 {
+            std::thread::sleep(std::time::Duration::from_millis(interval));
+        }
+
         format!(
             "Hi, {}!, from server-name: {}, Version: {}",
             name, server_name, version
